8a1c7f9d901807058826f810db3968ef653bf239
yash gupta <yashgupta1mole@gmail.com>
2023-08-04 06:19:04 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css';
 import RoutesFile from './RoutesFile'
 import Navbar from './Components/Navbar';
-import Form from './Components/Form';
-// import { useState } from 'react';
 function App() {
-
   return (
    <>
    <Navbar/>
